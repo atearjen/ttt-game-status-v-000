ea19f3cddace624f85e6d08f415da2dc2f993eee
Allison Tearjen <amtearjen@ucdavis.edu>
2018-09-09 17:57:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,40 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8], #bottom row
+  [0,3,6], #first columns
+  [1,4,7], #2nd columns
+  [2,5,8], #3rd columns
+  [0,4,8], #first diagonals
+  [2,4,6] #diagonal 2nd
+]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    not_emp = board.any? {|num| position_taken?(board,num.to_i) }
+  
+    if not_emp
+      if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
+        return win_combination
+      else
+        return false
+      end
+    else
+      return false
+    end #for if statement
+  end
+end
+
+def full?(board)
+  if board.all? {|i| i.position_taken?}
+    return true
+  else
+    return false
+  end
+end
