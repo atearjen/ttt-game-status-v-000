7e82d9d4fd4b35510ba866e731a01b53f07de9b7
Allison Tearjen <amtearjen@ucdavis.edu>
2018-09-09 18:22:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,17 +20,17 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    not_emp = board.any? {|num| position_taken?(board,num.to_i) }
+    # not_emp = board.any? {|num| position_taken?(board,num.to_i) }
 
-    if not_emp
-      if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
-        return win_combination
-      else
-        return false
-      end
+    # if not_emp
+    if board[win_index_1] == board[win_index_2] && board[win_index_1] == board[win_index_3]
+      return win_combination
     else
       return false
-    end #for if statement
+    end
+    # else
+      # return false
+    # end #for if statement
   end
 end
 
